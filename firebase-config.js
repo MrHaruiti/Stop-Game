82509d8f9a78ca1cd0a51614443f80a3fcb08876
MrHaruiti <haruitilucas@gmail.com>
2025-08-02 10:59:50 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SUA_APP_ID",
+  databaseURL: "https://SEU_PROJECT_ID.firebaseio.com"
+};
+
+firebase.initializeApp(firebaseConfig);
